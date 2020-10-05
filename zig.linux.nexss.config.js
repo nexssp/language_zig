2c57eed8133bfd68796acafb19885dfaa9691dac
Marcin Polak <mapoart@gmail.com>
2020-10-05 07:19:25 +0200

    Ubuntu 20+

--- zig.linux.nexss.config.js
+++ zig.linux.nexss.config.js
@@ -1,9 +1,11 @@
 let languageConfig = Object.assign({}, require("./zig.win32.nexss.config"));
-const os = require("@nexssp/os");
+const os = require(`${process.env.NEXSS_SRC_PATH}/node_modules/@nexssp/os/`);
+const sudo = os.sudo();
 const distName = os.name();
+const v = parseInt(os.v());
 languageConfig.compilers = {
   zig: {
-    install: os.replacePMByDistro("apt install -y ziglang"),
+    install: os.replacePMByDistro(`${sudo}apt install -y zig`),
     command: "zig",
     args: "run <file> --",
     help: ``,
@@ -11,12 +13,23 @@ languageConfig.compilers = {
 };
 
 switch (distName) {
-  case os.distros.ORACLE:
+  case os.distros.UBUNTU:
+    // from 20.04 install by snap
+    if (v >= 20) {
+      languageConfig.compilers.zig.install = `${sudo}snap install zig --classic --edge`;
+    }
+    break;
+  case os.distros.FEDORA:
     languageConfig.compilers.zig.install = os.replacePMByDistro(
-      languageConfig.compilers.zig.install
+      `${sudo}dnf install -y 'dnf-command(copr)'
+${sudo}dnf -y copr enable sentry/zig
+${sudo}dnf install -y zig zig-doc`
     );
     break;
   default:
+    languageConfig.compilers.zig.install = os.replacePMByDistro(
+      languageConfig.compilers.zig.install
+    );
     break;
 }
 
